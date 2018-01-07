8e854c161c98f11fce92393ae8693059a61ebf95
Jorge Yago <jorge.yago.meniz@gmail.com>
2018-01-07 19:33:36 +0100

    Iteration 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,9 +1,9 @@
 class Elevator {
   constructor(){
-    this.floor      = 0;
+    this.floor      = 1;
     this.MAXFLOOR   = 10;
     this.requests   = [];
-    this.direction = "up";
+    this.direction = "down";
   }
 
   start() {
@@ -19,8 +19,23 @@ class Elevator {
   }
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
+  floorUp() {
+    if (this.direction === "up" && this.floor < this.MAXFLOOR) {
+      this.floor++;
+      console.log(this.floor);
+    } else {
+      console.log(`The elevator cannot go higher than floor 10`);
+    }
+  }
+  floorDown() {
+    if (this.direction === "down" && this.floor > 0) {
+      this.floor--;
+      console.log(this.floor);
+    } else {
+      console.log(`The elevator cannot go lower than floor 0`);
+      // warning: if direction is differente from down, the error message displayed will be wrong
+    }
+  }
   call() { }
   log() {
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
@@ -28,6 +43,5 @@ class Elevator {
 }
 
 var myElevator =new Elevator();
-myElevator.start();
-myElevator.stop();
+myElevator.floorDown();
 module.exports = Elevator;
