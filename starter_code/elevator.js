fb50b895c5d958c55a5d447aea1d1bf54e755946
Jorge Yago <jorge.yago.meniz@gmail.com>
2018-01-07 20:07:31 +0100

    Iterarion 3

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,8 +1,12 @@
+let Person = require('./person.js');
+let person = new Person();
+
 class Elevator {
   constructor(){
     this.floor      = 1;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.floorsToServe = [];
     this.direction = "down";
   }
 
@@ -11,14 +15,17 @@ class Elevator {
       this.update();
     }, 1000);
   }
+
   stop() {
     clearInterval(this.interval);
   }
+
   update() {
     this.log();
   }
   _passengersEnter() { }
   _passengersLeave() { }
+
   floorUp() {
     if (this.direction === "up" && this.floor < this.MAXFLOOR) {
       this.floor++;
@@ -27,6 +34,7 @@ class Elevator {
       console.log(`The elevator cannot go higher than floor 10`);
     }
   }
+
   floorDown() {
     if (this.direction === "down" && this.floor > 0) {
       this.floor--;
@@ -36,12 +44,17 @@ class Elevator {
       // warning: if direction is differente from down, the error message displayed will be wrong
     }
   }
-  call() { }
+
+  call() {
+    this.requests.push(person);
+    console.log(this.requests);
+  }
+
   log() {
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
   }
 }
 
 var myElevator =new Elevator();
-myElevator.floorDown();
+myElevator.call();
 module.exports = Elevator;
